6647e5dc8a2a4e809d60ccdb94167ae8c61ceb36
Mario David <mario@die-davids.de>
2024-08-12 13:01:43 +0200

    file upload layout changes

--- src/main/java/io/jmix/windturbines/view/inspectionfinding/InspectionFindingDetailView.java
+++ src/main/java/io/jmix/windturbines/view/inspectionfinding/InspectionFindingDetailView.java
@@ -3,10 +3,13 @@ package io.jmix.windturbines.view.inspectionfinding;
 import com.vaadin.flow.component.Component;
 import com.vaadin.flow.component.html.Div;
 import com.vaadin.flow.component.orderedlayout.FlexLayout;
+import com.vaadin.flow.data.renderer.ComponentRenderer;
+import com.vaadin.flow.data.renderer.Renderer;
 import com.vaadin.flow.router.Route;
 import io.jmix.core.FileRef;
 import io.jmix.core.FileStorage;
 import io.jmix.flowui.DialogWindows;
+import io.jmix.flowui.Fragments;
 import io.jmix.flowui.Notifications;
 import io.jmix.flowui.UiComponents;
 import io.jmix.flowui.component.image.JmixImage;
@@ -21,10 +24,13 @@ import io.jmix.flowui.model.DataContext;
 import io.jmix.flowui.model.InstanceContainer;
 import io.jmix.flowui.upload.TemporaryStorage;
 import io.jmix.flowui.view.*;
+import io.jmix.windturbines.entity.Turbine;
 import io.jmix.windturbines.entity.inspection.InspectionFinding;
 import io.jmix.windturbines.entity.inspection.InspectionFindingEvidence;
+import io.jmix.windturbines.view.inspectionfinding.inspectionfindingevidencefragment.InspectionFindingEvidenceFragment;
 import io.jmix.windturbines.view.inspectionfindingevidence.InspectionFindingEvidenceDetailView;
 import io.jmix.windturbines.view.main.MainView;
+import io.jmix.windturbines.view.turbine.TurbineCard;
 import org.springframework.beans.factory.annotation.Autowired;
 
 import java.util.Optional;
@@ -38,19 +44,15 @@ public class InspectionFindingDetailView extends StandardDetailView<InspectionFi
     @Autowired
     private TemporaryStorage temporaryStorage;
     @Autowired
+    private Fragments fragments;
+    @Autowired
     private FileStorage fileStorage;
+
+
     @ViewComponent
     private CollectionPropertyContainer<InspectionFindingEvidence> evidencesDc;
     @ViewComponent
     private DataContext dataContext;
-    @Autowired
-    private UiComponents uiComponents;
-    @Autowired
-    private DataComponents dataComponents;
-    @ViewComponent
-    private Div evidencesListWrapper;
-    @Autowired
-    private DialogWindows dialogWindows;
     @ViewComponent
     private FileStorageUploadField upload;
 
@@ -70,76 +72,30 @@ public class InspectionFindingDetailView extends StandardDetailView<InspectionFi
             inspectionFindingEvidence.setInspectionFinding(getEditedEntity());
             evidencesDc.getMutableItems().add(inspectionFindingEvidence);
 
-            renderEvidenceList();
         }
     }
 
     @Subscribe
     public void onReady(final ReadyEvent event) {
-        renderEvidenceList();
         upload.setEnabled(!isReadOnly());
     }
 
-    private void renderEvidenceList() {
-        evidencesListWrapper.removeAll();
-        evidencesListWrapper.add(evidencesList());
+    @Supply(to = "evidencesVirtualList", subject = "renderer")
+    private Renderer<InspectionFindingEvidence> evidencesVirtualListRenderer() {
+        return new ComponentRenderer<>(this::createTurbineCard);
     }
 
-    private Component evidencesList() {
-
-        FlexLayout flexLayout = uiComponents.create(FlexLayout.class);
-        flexLayout.setFlexWrap(FlexLayout.FlexWrap.WRAP);
-        flexLayout.setAlignItems(FlexLayout.Alignment.START);
-        flexLayout.addClassName("evidence-flex-container");
-
-        Optional.ofNullable(getEditedEntity().getEvidences())
-                .ifPresent(evidences ->
-                        evidences.stream()
-                                .map(this::createEvidenceItem)
-                                .forEach(flexLayout::add)
-                );
-
-        return flexLayout;
-    }
-
-    private Component createEvidenceItem(InspectionFindingEvidence findingEvidence) {
-
-        InstanceContainer<InspectionFindingEvidence> instanceContainer = dataComponents.createInstanceContainer(InspectionFindingEvidence.class);
-        instanceContainer.setItem(findingEvidence);
-
-        JmixImage<FileRef> image = uiComponents.create(JmixImage.class);
-        image.setValueSource(new ContainerValueSource<>(instanceContainer, "file"));
-        image.addClassName("evidence-image");
-
-        Div imageContainer = uiComponents.create(Div.class);
-        imageContainer.addClassName("image-container");
-        imageContainer.add(image);
-
-        imageContainer.addClickListener(event -> {
-            DialogWindow<InspectionFindingEvidenceDetailView> dialogWindow = dialogWindows.detail(this, InspectionFindingEvidence.class)
-                    .withViewClass(InspectionFindingEvidenceDetailView.class)
-                    .editEntity(findingEvidence)
-                    .build();
-
-            dialogWindow.setWidthFull();
-            dialogWindow.setHeightFull();
-
-            dialogWindow.open();
+    private InspectionFindingEvidenceFragment createTurbineCard(InspectionFindingEvidence evidence) {
+        InspectionFindingEvidenceFragment fragment = fragments.create(this, InspectionFindingEvidenceFragment.class);
+        fragment.setEvidence(evidence);
+        fragment.setAfterAssignmentPerformedHandler(it -> {
+            evidencesDc.getMutableItems().remove(it);
+            dataContext.remove(dataContext.merge(it));
         });
+        return fragment;
+    }
 
-        if (!isReadOnly()) {
-            Div deleteEvidenceButton = new Div();
-            deleteEvidenceButton.addClassName("delete-button");
-            deleteEvidenceButton.setText("✕");
-            deleteEvidenceButton.addClickListener(e -> {
-                evidencesDc.getMutableItems().remove(findingEvidence);
-                dataContext.remove(findingEvidence);
-                renderEvidenceList();
-            });
-
-            imageContainer.add(deleteEvidenceButton);
-        }
-
-        return imageContainer;
+    public CollectionPropertyContainer<InspectionFindingEvidence> getEvidencesDc() {
+        return evidencesDc;
     }
 }
